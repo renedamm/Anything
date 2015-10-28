e49ae72badc8a253c21d2fd09423b2e3cd869a26
Rene Damm <rene.damm@gmx.net>
2015-10-28 19:06:54 +0100

    Misc changes.

--- Server/app/models/Token.scala
+++ Server/app/models/Token.scala
@@ -2,5 +2,7 @@ package models
 
 case class Token( text : String )
 
-case class TokenOccurrence( token : Token, file : File, line : Int, column : Int, position : Long )
+case class TokenUsageTag( text : String )
+
+case class TokenOccurrence( token : Token, usage : TokenUsageTag, file : File, line : Int, column : Int, position : Long )
 
---
+++ Server/test/ProjectsSpec.scala
@@ -0,0 +1,13 @@
+import org.specs2.mutable._
+//import org.specs2.runner._
+//import org.junit.runner._
+
+//@RunWith( classOf[ JUnitRuner ] )
+class ProjectsSpec
+    extends Specification
+{
+	//"Projects" should
+	//{
+	//}
+}
+
